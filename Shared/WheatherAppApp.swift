c7ec036f0d46e0fc982d2273e010627456fa28ec
Ryan <ryan.wennekes@hotmail.com>
2022-02-09 14:13:01 +0100

    Initial Commit

---
+++ Shared/WheatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheatherAppApp.swift
+//  Shared
+//
+//  Created by Ryan Wennekes on 09/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WheatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
